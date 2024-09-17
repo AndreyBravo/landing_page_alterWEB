7595372baf15cf47596c7cb89a2442993a6291d0
Andrey Babichev <klopipa@gmail.com>
2024-09-17 16:28:43 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,266 @@
+const paginationNumbers = document.getElementById("pagination-numbers");
+const paginatedList = document.getElementById("paginated-list");
+const listItems = paginatedList.querySelectorAll("li");
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+
+const paginationLimit = 1;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    button.classList.remove("is-active");
+    const pageIndex = Number(button.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      button.classList.add("is-active");
+    }
+  });
+};
+
+const appendPageNumber = (index) => {
+  const pageNumber = document.createElement("button");
+  pageNumber.className = "pagination-number";
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute("page-index", index);
+  pageNumber.setAttribute("aria-label", "Page " + index);
+
+  paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+const setCurrentPage = (pageNum) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();
+  handlePageButtonsStatus();
+
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  listItems.forEach((item, index) => {
+    item.classList.add("is-hidden");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("is-hidden");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+
+  prevButton.addEventListener("click", () => {
+    setCurrentPage(currentPage - 1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    setCurrentPage(currentPage + 1);
+  });
+
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    const pageIndex = Number(button.getAttribute("page-index"));
+
+    if (pageIndex) {
+      button.addEventListener("click", () => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
+
+// modal
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const stop = false;
+
+function closeModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+//input file
+function uploadFile(target) {
+  document.getElementById("file-name").innerHTML = target.files[0].name;
+}
+
+//scrollOnVacancy
+document.querySelectorAll('a[href^="#"').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let href = this.getAttribute("href").substring(1);
+
+    const scrollTarget = document.getElementById(href);
+    const header = document.getElementById("myHeader");
+    let topOffset;
+    if (header.classList.contains("sticky")) {
+      topOffset = document.querySelector(".scrollto").offsetHeight;
+    } else {
+      topOffset = 150;
+    }
+    //const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  });
+});
+//form
+
+const modal_input = document.getElementsByClassName("modal-input");
+console.log(modal_input);
+modal_input.onchange = console.log(modal_input.value);
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const form = document.getElementById("form");
+//   form.addEventListener("submit", formSend);
+
+//   async function formSend(e) {
+//     e.preventDefault();
+
+//     let error = formValidate(form);
+
+//     let formDate = new FormData(form);
+//     formDate.append("file", formFile.files[0]);
+//     if (error === 0) {
+//       form.classList.add("_sending");
+//       // let response = await fetch('sendmail.php',{
+
+//       //   method:"POST",
+//       //   body:formDate
+//       // })
+//       if (response.ok) {
+//         let result = await response.json();
+//         form.reset();
+//         form.classList.remove("_sending");
+//       } else {
+//         form.classList.remove("_sending");
+//       }
+//     }
+//   }
+
+//   function changeValidate(form) {
+//     let error = 0;
+//     let formReq = document.querySelectorAll("._req");
+
+//     for (let index = 0; index < formReq.length; index++) {
+//       const input = formReq[index];
+//       formRemoveError(input);
+
+//       if (input.classList.contains("phone")) {
+//         if (emailTest(input)) {
+//           formAddError(input);
+//           error++;
+//         }
+//       } else if (
+//         input.getAttribute("type") === "checkbox" &&
+//         input.checked === false
+//       ) {
+//         formAddError(input);
+//         error++;
+//       } else {
+//         if (input.value === "") {
+//           formAddError(input);
+//           error++;
+//         }
+//       }
+//     }
+//     return error;
+//   }
+//   function formValidate(form) {
+//     let error = 0;
+//     let formReq = document.querySelectorAll("._req");
+//     form.addEventListener("click", changeValidate);
+//     for (let index = 0; index < formReq.length; index++) {
+//       const input = formReq[index];
+//       formRemoveError(input);
+
+//       if (input.classList.contains("phone")) {
+//         if (emailTest(input)) {
+//           formAddError(input);
+//           error++;
+//         }
+//       } else if (
+//         input.getAttribute("type") === "checkbox" &&
+//         input.checked === false
+//       ) {
+//         formAddError(input);
+//         error++;
+//       } else {
+//         if (input.value === "") {
+//           formAddError(input);
+//           error++;
+//         }
+//       }
+//     }
+//     return error;
+//   }
+
+function formAddError(input) {
+  input.parentElement.classList.add("is-error");
+  input.classList.add("is-error");
+}
+
+function formRemoveError(input) {
+  input.parentElement.classList.remove("is-error");
+  input.classList.remove("is-error");
+}
+
+//   function emailTest(input) {
+//     console.log(input);
+//     return "".test(input.value);
+//   }
+
+//   // function emailTest(input) {
+//   //   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+//   // }
+
+//   // const formFile = document.getElementById("file");
+//   // formFile.addEventListener("change", () => {
+//   //   formFile.files[0];
+//   // });
+// });
